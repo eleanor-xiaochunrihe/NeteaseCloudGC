2a8fab46c291853b01b3dceed118a40afa219de6
eleanor <Eleanorxcrh@outlook.com>
2025-03-18 00:42:33 +0800

    tryto初始化

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -59,10 +59,10 @@
   </view>
 </template>
 
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import { ref } from 'vue'
-const title = ref('Hello')
-</script>
+const title = ref('Hello') -->
+<!-- </script> -->
 
 <style>
 .content {
